e71859a6c70c4a581d8af628815b2b0854984421
曹琛 <caochen_workmail@foxmail.com>
2018-04-19 19:59:13 +0800

    Dioooooooor
    1.将代码模块化

--- spider.py
+++ spider.py
@@ -57,36 +57,74 @@ user_agents = [
                    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 ",    
                    ]   
 
-res_tbody = r'<tbody>(.*?)</tbody>'
-res_tr = r'<tr class="(.*?)">(.*?)</tr>'
-res_td = r'<td>(.*?)</td>'
+history = {
+    'res_tbody': r'<tbody>(.*?)</tbody>',
+    'res_tr': r'<tr class="(.*?)">(.*?)</tr>',
+    'res_td': r'<td>(.*?)</td>'
+}
+
+realTime = {
+    'res_table': r'<table cellpadding="1" cellspacing="1" border="0">(.*?)</table>',
+    'res_tbody': r'<tbody>(.*?)</tbody>',
+    'res_tr': r'<tr>(.*?)</tr>',
+    'res_td': r'<td>(.*?)</td>',
+    #'res_ul': r'<ul class="(.*?)">(.*?)</ul>'
+    'res_i': r'<i>(.*?)</i>'
+}
+
 
 #爬取入口
 def main(url, i):
+    dbName = "lotteryticket"
     htmlTools = HtmlTools()
     accessTools = AccessTools()
     html = htmlTools.getHtmlInfo(url)
-    Data = ResolveToData(html)
-    for data in Data:
-        ProcessData(data)
-    accessTools.WriteToText(Data)
+    print(html)
+    Data = ResolveToData_RealTime(html)
+    # accessTools.initMySql("localhost", "root", "caochen520")
+    # for data in Data:
+    #     ProcessData(data)
+    #     #写入MySql
+    #     sql = GenerateSql(data)
+    #     accessTools.WriteToMySql(dbName, sql)
+    #写入Json
+    #accessTools.WriteToText(Data)
     # PrizeInformation = getPrizeInformation(m_tr)
     # AllInformation[i] = PrizeInformation
     #print(AllInformation)
 
-#解析界面，提取数据
-def ResolveToData(html):
+#解析界面，提取数据     http://www.bwlc.net/bulletin/trax.html 站点信息
+def ResolveToData_History(html):
     Data = []
-    m_tr = re.findall(res_tr,html,re.S|re.M)
+    m_tr = re.findall(history['res_tr'],html,re.S|re.M)
     for line in m_tr:
-        m_td = re.findall(res_td,line[1],re.S|re.M)
+        m_td = re.findall(history['res_td'],line[1],re.S|re.M)
         Data.append(m_td)
     return Data
 
+#解析界面，提取数据   https://www.38011.com/kjls/pk10/pk10kai_history.html 站点信息
+def ResolveToData_RealTime(html):
+    Data = []
+    m_table = re.findall(realTime['res_table'],html,re.S|re.M)
+    print(m_table)
+    m_tbody = re.findall(realTime['res_tbody'],m_table,re.S|re.M)
+    print(m_tbody)
+    m_tr = re.findall(realTime['res_tr'],m_tbody,re.S|re.M)
+    print(m_tr)
+    for _tr in m_tr:
+        m_td = re.findall(realTime['res_td'],_tr,re.S|re.M)
+        if m_td == None:
+            continue
+        i = re.findall(realTime['res_i'],m_td,re.S|re.M)
+        print(i)
+        # data = []
+        # data.append(m_td[0],m_td[1],m_td[2])
+    return Data
+
 #处理数据，添加属性
-def ProcessData(Data):
+def ProcessData(data):
     Property = {}
-    prizeNumber = ''.join(Data[1])
+    prizeNumber = ''.join(data[1])
     NumberList = prizeNumber.split(',')
     count = 1
     for n in NumberList:
@@ -95,35 +133,59 @@ def ProcessData(Data):
         else:
             Property[n] = 1
         count += 1
-    Data.append(Property)
+    data.append(Property)
 
-#获取某一期信息
-def getPrizeInformation(m_tr):
-    PrizeInformation = []
-    for line in m_tr:
-        m_td = re.findall(res_td,line[1],re.S|re.M)
-        print(m_td)
-        connection = pymysql.connect(host='localhost',
-                        user='root',
-                        password='caochen520',
-                        db='lotteryticket',
-                        charset='utf8',
-                        cursorclass=pymysql.cursors.DictCursor)
-        cursor = connection.cursor()
-        sql = "INSERT INTO info__oper (期号, 中奖号码, 中奖时间) VALUES ('%s', '%s', '%s')"%(m_td[0], m_td[1], m_td[2])
-        cursor.execute(sql)
-        cursor.close()
-        connection.commit()
-        time.sleep(1)
-        dic = {'期号':m_td[0],'中奖号码':m_td[1],'开奖时间':m_td[2]}
-        PrizeInformation.append(dic)    
-        Issue.append(m_td[0])
-        PrizeNum.append(m_td[1])
-        Date.append(m_td[2])
-    return PrizeInformation
+def GenerateSql(data):
+    sql = "INSERT INTO info_oper (期号, 中奖号码, 中奖时间, \
+                                  一, \
+                                  二, \
+                                  三, \
+                                  四, \
+                                  五, \
+                                  六, \
+                                  七, \
+                                  八, \
+                                  九, \
+                                  十) \
+                                  VALUES ('%s', '%s', '%s', '%d', '%d', '%d', '%d', '%d', '%d', '%d', '%d', '%d', '%d')"%(
+                                      data[0], data[1], data[2], 
+                                      data[3]["01"],data[3]["02"],data[3]["03"],data[3]["04"],data[3]["05"],data[3]["06"],data[3]["07"],data[3]["08"],data[3]["09"],data[3]["10"])
+    print(sql)
+    print('\n')
+    return sql
+
+# #获取某一期信息
+# def getPrizeInformation(m_tr):
+#     PrizeInformation = []
+#     for line in m_tr:
+#         m_td = re.findall(res_td,line[1],re.S|re.M)
+#         print(m_td)
+#         connection = pymysql.connect(host='localhost',
+#                         user='root',
+#                         password='caochen520',
+#                         db='lotteryticket',
+#                         charset='utf8',
+#                         cursorclass=pymysql.cursors.DictCursor)
+#         cursor = connection.cursor()
+#         sql = "INSERT INTO info__oper (期号, 中奖号码, 中奖时间) VALUES ('%s', '%s', '%s')"%(m_td[0], m_td[1], m_td[2])
+#         cursor.execute(sql)
+#         cursor.close()
+#         connection.commit()
+#         time.sleep(1)
+#         dic = {'期号':m_td[0],'中奖号码':m_td[1],'开奖时间':m_td[2]}
+#         PrizeInformation.append(dic)    
+#         Issue.append(m_td[0])
+#         PrizeNum.append(m_td[1])
+#         Date.append(m_td[2])
+#     return PrizeInformation
+
+url = "https://www.38011.com/1/pk10/pk10list.php?lotCode=10001"
 
 if __name__ == "__main__":
-    main("http://www.bwlc.net/bulletin/prevtrax.html",1)
+    historyPage = "http://www.bwlc.net/bulletin/trax.html"
+    ChildPage = "http://www.bwlc.net/bulletin/prevtrax.html"
+    realTimePage = "https://www.38011.com/kjls/pk10/pk10kai_history.html"
+    main(realTimePage,1)
     # threads = []
     # for i in range(1, 15000):
     #     urls = "http://www.bwlc.net/bulletin/prevtrax.html?page=%d" %i
