2ba8d8d7a911361abb8823e34663adb3e15340a9
曹琛 <caochen_workmail@foxmail.com>
2018-04-23 18:55:40 +0800

    Dioooooooor
    1.初步完成爬取和提醒功能，但是不能实时爬取，要换一个网站

--- HtmlTools.py
+++ HtmlTools.py
@@ -6,8 +6,8 @@ class HtmlTools:
     def __init__(self):
         return
 
-    def getHtmlInfo(self, _url):
-        req = urllib.request.Request(url = _url)
+    def getHtmlInfo(self, _url, _headers):
+        req = urllib.request.Request(url = _url, headers = _headers)
         html = request.urlopen(req).read()
         html = html.decode('utf-8')
         return html
--- homeSpider.py
+++ homeSpider.py
@@ -12,24 +12,16 @@ import threading
 import random
 import time
 import json
-#import pymysql.cursors
+import win32api
+import win32gui
+import win32con
+from HtmlTools import HtmlTools
 
-'''数据注释
-AllInformation    所有的信息
-PrizeInformation  某一页的信息
-Issue             期号
-PrizeNum          中奖号码
-Date              开奖时间
-'''
+ip_url = 'http://www.xicidaili.com/nn/'
+url_mtc = "https://www.mtc328.com/data/bjpk10/lotteryList/%s.json"%(time.strftime('%Y-%m-%d',time.localtime(time.time())))
+url_38011 = "https://www.38011.com/1/pk10/pk10list.php?lotCode=10001"
 
-AllInformation = {}
-PrizeInformation = []
-Issue = []                                      
-PrizeNum = []                                  
-Date = []                                       
-
-
-url = "https://www.mtc328.com/data/bjpk10/lotteryList/2018-04-22.json"
+Proxy = []
 
 User_Agent = [  
                 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',  
@@ -44,30 +36,56 @@ User_Agent = [
 
 headers = {"User-Agent": random.choice(User_Agent)}
 
+latestPrizeNum = []
+
 #爬取入口
-def main(url):
-    data = getHtmlInfo(url)
-    ticketJson = json.loads(data)
+def main():
+    data_mtc = getHtmlInfo(url_mtc)
+    data_38011 = getHtmlInfo(url_38011)
+    ticketJson_mtc = json.loads(data_mtc)
+    ticketJson_38011 = json.loads(data_38011)
+    print(ticketJson_38011)
+    openMinute = time.strptime(ticketJson_mtc[0]['openDateTime'], "%Y-%m-%d %H:%M:%S").tm_min
+    nowMinute = time.localtime().tm_min
+    if abs(nowMinute - openMinute) >= 6:
+        win32api.MessageBox(win32con.NULL, "拉取数据出现问题", '你好', win32con.MB_OK) 
+        print("当前开奖信息为： \
+                期号： %s,\
+                中奖号码： %s,\
+                开奖时间： %s"\
+                %(ticketJson_mtc[0]['issue'],ticketJson_mtc[0]['openNum'],ticketJson_mtc[0]['openDateTime']))
+        global runFlag 
+        runFlag = False
+        return
+    global latestPrizeNum
+    tmpPrizeNum = ticketJson_mtc[0]['openNum']
+    if tmpPrizeNum == latestPrizeNum:
+        return
+    latestPrizeNum = tmpPrizeNum
     print("最新开奖信息为: \
             期号： %s,\
             中奖号码： %s,\
             开奖时间： %s"\
-            %(ticketJson[0]['issue'],ticketJson[0]['openNum'],ticketJson[0]['openDateTime']))
+            %(ticketJson_mtc[0]['issue'],ticketJson_mtc[0]['openNum'],ticketJson_mtc[0]['openDateTime']))
     ticketData = []
-    for tj in ticketJson:
+    for tj in ticketJson_mtc:
         AddAttributes(tj)
         ticketData.append(ResolveToData(tj))
     result = getResults(ticketData)
+    resultStr = ''
     for i in range(1,11):
         if result[str(i)] >= 5:
             if ticketData[0]['Attributes'][str(i)] == 0:
-                print("建议购买 号码为：后%d，连续次数为： %d"%(i,result[str(i)]))
+                resultStr += "建议购买 号码为：后%d，连续次数为： %d \n"%(i,result[str(i)])
+                # win32api.MessageBox(win32con.NULL, "建议购买 号码为：后%d，连续次数为： %d"%(i,result[str(i)]), '中奖信息', win32con.MB_OK) 
+                # print("建议购买 号码为：后%d，连续次数为： %d"%(i,result[str(i)])) 
             else:
-                print("建议购买 号码为：前%d，连续次数为： %d"%(i,result[str(i)]))
-    #print(result)
-    
-    #print(ticketJson)
-    #print(AllInformation)
+                resultStr += "建议购买 号码为：前%d，连续次数为： %d \n"%(i,result[str(i)])
+                # win32api.MessageBox(win32con.NULL, "建议购买 号码为：前%d，连续次数为： %d"%(i,result[str(i)]), '中奖信息', win32con.MB_OK)  
+                # print("建议购买 号码为：前%d，连续次数为： %d"%(i,result[str(i)])) 
+    if resultStr != '':
+        print(resultStr)
+        win32api.MessageBox(win32con.NULL, resultStr, '中奖信息', win32con.MB_OK) 
 
 def ResolveToData(json):
     data = {}
@@ -102,10 +120,32 @@ def getResults(data):
 
 #获取页面信息
 def getHtmlInfo(_url):
-    req = urllib.request.Request(url = _url, headers=headers)
-    html = request.urlopen(req).read()
-    data = html.decode('utf-8')
+    tmpProxy = {'http': random.choice(Proxy)}
+    proxy_support = request.ProxyHandler(tmpProxy)
+    opener = request.build_opener(proxy_support)
+    opener.addheaders = [('User-Agent',random.choice(User_Agent))]
+    request.install_opener(opener)
+    response = request.urlopen(_url)
+    data = response.read().decode('utf-8')
     return data
 
+def get_ip_list(url):
+    htmlTools = HtmlTools()
+    web_data = htmlTools.getHtmlInfo(url, headers)
+    soup = BeautifulSoup(web_data, 'lxml')
+    ips = soup.find_all('tr')
+    ip_list = []
+    for i in range(1, len(ips)):
+        ip_info = ips[i]
+        tds = ip_info.find_all('td')
+        ip_list.append(tds[1].text + ':' + tds[2].text)
+    return ip_list
+
 if __name__ == "__main__":
-    main(url)
+    for ip in get_ip_list(ip_url):
+        Proxy.append('http://'+ip)
+    runFlag = True
+    while runFlag:
+        main()
+        time.sleep(5)
+        pass
